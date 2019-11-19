0cd5b202e9492ee292b7b3c6dba8f0b96fcab289
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-11-19 20:30:16 +0000

    Added EventException

--- Increment.ps1
+++ Increment.ps1
@@ -6,8 +6,10 @@ $incrementVersionScriptPath = "$PSScriptRoot\.\Increment-Version.ps1"
 
 $updateVersionScriptPath = "$PSScriptRoot\.\UpdateVersion-Powershell.ps1"
 
+$dotnetPackScript = "$PSScriptRoot\.\DotNet-Pack.ps1"
+
 $currentVersion = & $getVersionScriptPath -fileName $solutionProperties
 
 $newVersion = & $incrementVersionScriptPath -versionString $currentVersion -versionPartIndex 3
 
-& $updateVersionScriptPath -fileName $solutionProperties -version $newVersion
+& $dotnetPackScript -version $newVersion
