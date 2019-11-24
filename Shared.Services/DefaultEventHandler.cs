857ad6c91f5fb459493ad9af6b7b0eacc72e9b8c
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-11-24 18:10:11 +0000

    Added command switch helper built-in

--- Shared.Services/DefaultEventHandler.cs
+++ Shared.Services/DefaultEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Shared.Contracts;
+using System;
 using System.Threading.Tasks;
 
 namespace Shared.Services
@@ -7,17 +8,34 @@ namespace Shared.Services
         where TEvent : IEvent
     {
         public abstract Task<TEvent> Push(TEvent @event);
+        
+        public virtual async Task<TEvent> Send<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            var foundCommand = GetCommand(command.Name);
 
-        public abstract Task<TEvent> Send<TCommand>(TCommand command) where TCommand : ICommand;
+            if(foundCommand == null)
+                throw new MethodAccessException();
+
+            return await foundCommand(command);
+        }
 
         public async Task<IEvent> Push(IEvent @event)
         {
             return await Push((TEvent)@event);
         }
+        
+        protected ISwitch<string, Func<ICommand, Task<TEvent>>> CommandSwitch;
+        protected virtual Func<ICommand, Task<TEvent>> GetCommand(string commandName) => CommandSwitch.Case(commandName);
 
         async Task<IEvent> IEventHandler.Send<TCommand>(TCommand command)
         {
             return await Send(command);
+        }
+
+        protected DefaultEventHandler()
+        {
+            CommandSwitch = DefaultSwitch
+                .Create<string, Func<ICommand, Task<TEvent>>>();
         }
     }
 }
