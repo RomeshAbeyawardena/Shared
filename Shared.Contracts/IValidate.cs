02f6d44c239e4c3704ebebb24eca799b260e3b12
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-12-11 11:35:09 +0000

    Implemented validation fluent API

--- Shared.Contracts/IValidate.cs
+++ Shared.Contracts/IValidate.cs
@@ -5,7 +5,8 @@ namespace Shared.Contracts
     public interface IValidate<T>
     {
         IValidate<T> IsNotNull<TMember>(Func<T, TMember> member);
-        IValidate<T> IsInRange<TMember>(Func<T, TMember> member, Func<TMember, TMember, bool> isInRangeComparer);
-        IValidate<T> IsEqual<TMember>(Func<T, TMember> member, Func<TMember, TMember, bool> equalityComparer);
+        IValidate<T> IsInRange<TMember>(Func<T, TMember> member, TMember minimumValue, 
+            TMember maximumValue, Func<TMember, TMember, TMember, bool> isInRangeComparer);
+        IValidate<T> IsValid<TMember>(Func<T, TMember> member, TMember value, Func<TMember, TMember, bool> equalityComparer);
     }
 }
--- Shared.Library/Validate.cs
+++ Shared.Library/Validate.cs
@@ -1,4 +1,5 @@
 using Shared.Contracts;
+using Shared.Library.Exceptions;
 using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Library
@@ -16,20 +17,31 @@ namespace Shared.Library
         {
             var member = getMember(_model);
 
-            throw new Va
+            var memberName = nameof(member);
+            if(member == null)
+                throw new ValidateException(memberName, $"{memberName} must not be null");
             
             return this;
         }
 
-        public IValidate<T> IsInRange<TMember>(System.Func<T, TMember> getMember, System.Func<TMember, TMember, bool> isInRangeComparer)
+        public IValidate<T> IsValid<TMember>(System.Func<T, TMember> getMember, TMember value, System.Func<TMember, TMember, bool> equalityComparer)
         {
             var member = getMember(_model);
+            var memberName = nameof(member);
+
+            if(!equalityComparer(member, value))
+                throw new ValidateException(memberName, $"{memberName} invalid");
             return this;
         }
 
-        public IValidate<T> IsEqual<TMember>(System.Func<T, TMember> getMember, System.Func<TMember, TMember, bool> equalityComparer)
+        public IValidate<T> IsInRange<TMember>(System.Func<T, TMember> getMember, TMember minimumValue, TMember maximumValue, System.Func<TMember, TMember, TMember, bool> isInRangeComparer)
         {
             var member = getMember(_model);
+            var memberName = nameof(member);
+
+            if(!isInRangeComparer(member, minimumValue, maximumValue))
+                throw new ValidateException(memberName, $"{memberName} is not within the range of valid values");
+
             return this;
         }
     }
